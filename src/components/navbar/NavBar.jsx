0f02355c32e80029413e03c7d16f6c8d464faada
hustarico <hussamtarteer74@gmail.com>
2026-02-17 15:19:47 +0300

    starting with nav

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,10 @@
+import './NavBar.css'
+
+export default function NavBar(){
+
+    return <div className='navbar'>
+        <h1 className='pointer' style={{margin:0}}>testing</h1>
+        <h1 className='pointer' style={{margin:0}}>testing</h1>
+        <h1 className='pointer' style={{margin:0}}>testing</h1>
+    </div>
+}
